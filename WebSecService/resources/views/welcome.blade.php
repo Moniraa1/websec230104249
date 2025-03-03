1ca367f69d3f3b013d6dcb8a7d6c4dc6a971fa93
Moniraa1 <moniramahmoud0@gmail.com>
2025-03-03 10:41:49 +0200

    code

---
+++ WebSecService/resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.master') 
+@section('title', 'welcome') 
+@section('content')
+    <div class="card m-4">
+    <div class="card-body">
+        welcome to home page
+    </div>
+    </div>
+@endsection
---
+++ WebSecService/routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/multable', function () {
+    return view('multable'); //multable.blade.php
+});
+
+Route::get('/even', function () {
+    return view('even'); //even.blade.php
+});
+    
+Route::get('/prime', function () {
+    return view('prime'); //prime.blade.php
+}); 
